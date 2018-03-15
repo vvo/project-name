c78f9b314a47d28b6d8d04cb0d0d435798f5288d
vvo <vincent.voyer@algolia.com>
2018-03-15 10:33:29 +0100

    feat(public): it works

--- src/public-module/src/index.js
+++ src/public-module/src/index.js
@@ -2,5 +2,5 @@ const privateModule = require('private-module');
 
 module.exports = function publicModule() {
   // eslint-disable-next-line no-console
-  console.log(privateModule('library'));
+  console.log(privateModule('library works'));
 };
